b0f36e3b52e5333ef610e821b5d04d706d97edcd
koreannewB <joeynoh2002@naver.com>
2026-06-20 20:28:56 +0900

    oneec2-1

--- terraform-01-ec2-webserver/user-data.sh
+++ terraform-01-ec2-webserver/user-data.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-set -euxo pipefail
+set -eux
 
 dnf update -y
-dnf install -y nginx
+dnf install -y git python3 python3-pip
 
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-AVAILABILITY_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+cd /home/ec2-user
 
-cat > /usr/share/nginx/html/index.html <<EOF
-<h1>Terraform Example 01</h1>
-<p>Role: Single EC2 web server</p>
-<p>Name Prefix: ${name_prefix}</p>
-<p>Instance ID: $${INSTANCE_ID}</p>
-<p>Availability Zone: $${AVAILABILITY_ZONE}</p>
-EOF
+git clone https://github.com/koreannewB/aws_iot_32237277.git
 
-systemctl enable nginx
-systemctl restart nginx
+cd aws_iot_32237277
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+nohup python3 main.py > smartgym.log 2>&1 &
